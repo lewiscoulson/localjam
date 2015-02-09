ee6a80ee17b89a20ee466895a5a53ac2aac6a8d2
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-02-09 17:42:17 +0000

    show lessons

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 angular.module('localjam', [])
     .controller('mainCtrl', function($scope) {
-        $scope.hello = 'world';
+        $scope.lessons = [{"id":10,"title":"lesson 1","video":"video.mov","notes":"lesson notes","created_at":"2015-02-06T15:36:49.853Z","updated_at":"2015-02-06T15:36:49.853Z"},{"id":11,"title":"lesson 2","video":"vid.mov","notes":"lesson 2 notes","created_at":"2015-02-06T15:38:58.894Z","updated_at":"2015-02-06T15:38:58.894Z"},{"id":12,"title":"lesson 3","video":"3.mov","notes":"lesson 3 notes","created_at":"2015-02-06T16:41:16.861Z","updated_at":"2015-02-06T16:41:16.861Z"}]
     });
 
 
