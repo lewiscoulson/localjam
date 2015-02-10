8bdaeacb426234df8b680d8e2ca316b9ab17a6a5
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-02-10 15:40:24 +0000

    down and dirty prototype

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,13 +12,31 @@ angular.module('localjam', [])
     		$scope.activeLesson = lesson;
     		$scope.isShowLesson = true;
     		$scope.isShowLessons = false;
+            $scope.isShowContact = false;
+            $scope.isShowAbout = false;
     	}
 
     	function showLessons() {
     		$scope.activeLesson = null;
     		$scope.isShowLesson = false;
     		$scope.isShowLessons = true;
+            $scope.isShowContact = false;
+            $scope.isShowAbout = false;
     	}
+
+        function showAbout() {
+            $scope.isShowLesson = false;
+            $scope.isShowLessons = false;
+            $scope.isShowContact = false;
+            $scope.isShowAbout = true;
+        }
+
+        function showContact() {
+            $scope.isShowLesson = false;
+            $scope.isShowLessons = false;
+            $scope.isShowAbout = false;
+            $scope.isShowContact = true;
+        }
 
     	function init() {
     		$scope.isShowLessons = true;
@@ -26,6 +44,8 @@ angular.module('localjam', [])
 
     	$scope.showLesson = showLesson;
     	$scope.showLessons = showLessons;
+        $scope.showAbout = showAbout;
+        $scope.showContact = showContact;
 
     	init();
     });
